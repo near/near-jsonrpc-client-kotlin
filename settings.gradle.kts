4d5cb931ba23853366d7c20716a5bdd39883c156
hossein <h.karami1811@gmail.com>
2026-06-19 23:48:26 +0330

    fix: platform targets updated

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,39 +3,20 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        maven("https://en-mirror.ir")
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/public")
-//        google {
-//            mavenContent {
-//                includeGroupAndSubgroups("androidx")
-//                includeGroupAndSubgroups("com.android")
-//                includeGroupAndSubgroups("com.google")
-//            }
-//        }
         google()
         mavenCentral()
         gradlePluginPortal()
-        //maven { setUrl("https://maven.myket.ir") }
     }
 }
 
 dependencyResolutionManagement {
     repositories {
-        maven("https://en-mirror.ir")
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/public")
-        maven("https://jitpack.io")
         google()
-//        google {
-//            mavenContent {
-//                includeGroupAndSubgroups("androidx")
-//                includeGroupAndSubgroups("com.android")
-//                includeGroupAndSubgroups("com.google")
-//            }
-//        }
         mavenCentral()
-        //maven { setUrl("https://maven.myket.ir") }
     }
 }
 
