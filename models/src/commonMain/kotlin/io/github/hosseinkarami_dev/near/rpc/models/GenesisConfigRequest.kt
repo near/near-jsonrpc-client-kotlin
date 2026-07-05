06981da44d8b66c34dfc5286320ccb5dfade4bc6
Hossein Karami <h.karami1811@gmail.com>
2026-07-05 22:31:09 +0330

    chore: regenerate client from OpenAPI (#138)
    Co-authored-by: GitHub Actions Bot <automation@github.com>

--- models/src/commonMain/kotlin/io/github/hosseinkarami_dev/near/rpc/models/GenesisConfigRequest.kt
+++ models/src/commonMain/kotlin/io/github/hosseinkarami_dev/near/rpc/models/GenesisConfigRequest.kt
@@ -1,5 +1,9 @@
 package io.github.hosseinkarami_dev.near.rpc.models
 
-import kotlinx.serialization.json.JsonElement
+import kotlin.Unit
 
-public typealias GenesisConfigRequest = JsonElement?
+/**
+ *  * Nullable: true
+ *  * Possible values: null
+ */
+public typealias GenesisConfigRequest = Unit?
--- models/src/commonMain/kotlin/io/github/hosseinkarami_dev/near/rpc/models/RpcClientConfigRequest.kt
+++ models/src/commonMain/kotlin/io/github/hosseinkarami_dev/near/rpc/models/RpcClientConfigRequest.kt
@@ -1,5 +1,9 @@
 package io.github.hosseinkarami_dev.near.rpc.models
 
-import kotlinx.serialization.json.JsonElement
+import kotlin.Unit
 
-public typealias RpcClientConfigRequest = JsonElement?
+/**
+ *  * Nullable: true
+ *  * Possible values: null
+ */
+public typealias RpcClientConfigRequest = Unit?
